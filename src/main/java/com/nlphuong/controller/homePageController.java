2a33b6cb2ffc231df897313bc8f7c3c75a7e2379
NguyenLinhPhuong <pnguyen9968@gmail.com>
2021-08-08 17:23:37 +0700

    task#02 return type from controller to view(jsp file) by ModelAndView, ModelMap

--- src/main/java/com/nlphuong/controller/homePageController.java
+++ src/main/java/com/nlphuong/controller/homePageController.java
@@ -11,19 +11,44 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.nlphuong.entity.Director;
 import com.nlphuong.entity.Employee;
 
 @Controller
 public class homePageController {
+	
+	//Cach 1
+/*	@RequestMapping("/")
+	public ModelAndView homePage() {
+		ModelAndView viewHomePage = new ModelAndView();
+		viewHomePage.setViewName("homePage");
+		
+		String username = "Nguyen Linh Phuong";
+		String password = "123456";
+		
+		viewHomePage.addObject("username", 1);
+		viewHomePage.addObject("password", password);
+		
+		return viewHomePage;
 
-	@RequestMapping("/")
-	public String homePage() {
-
-		return "homePage";
 	}
+*/
+	//Cach 2
+	@RequestMapping("/")
+	public String homePage(ModelMap modelMap) {
+		
+		String username = "Nguyen Linh Phuong";
+		String password = "123456";
+		
+		modelMap.addAttribute("username", username);
+		modelMap.addAttribute("password", password);
+		
+		return "homePage";
 
+	}
 }
